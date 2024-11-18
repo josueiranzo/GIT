96ab701a5b84b20f0a395faad0fbbdfc5eb912f6
A36-PC26\alumnat <a@a.com>
2024-11-18 16:58:44 +0100

    Create practica.java

---
+++ practica.java
@@ -0,0 +1,26 @@
+package main;
+
+public class practica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] sumArray = {4,36,4,23,867};
+		int[] subArray = {785675, 56, 5675, 56544, 234};
+		int[] prodArray = {6, 8, 20, 23, 12};
+		
+		// User 1 fer la suma dels elements de sumArray
+		int sum = 0;
+		
+		for (int i = 0; i < sumArray.length; i++) {
+			sum += sumArray[i;]
+		}
+	
+		
+		// User 2 fer la resta dels elements de subArray
+		
+		
+		
+		// User 3 fer la multiplicació dels elements de prodArray
+	}
+
+}
